c90eca18fa715cbece1e5861f07acc898baca412
James Babineau <babineau.james@gmail.com>
2017-10-11 00:23:32 -0400

    initial project setup

---
+++ src/main/java/com/jbabineau/commuter/CommuterApplication.java
@@ -0,0 +1,12 @@
+package com.jbabineau.commuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuterApplication.class, args);
+	}
+}
