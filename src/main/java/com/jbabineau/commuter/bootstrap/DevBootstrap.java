91f3e5f508df2db715b6f5701ceaa61b33f6f0e6
James Babineau <babineau.james@gmail.com>
2017-10-12 17:22:24 -0400

    Adding People to the Dev bootstrap

--- src/main/java/com/jbabineau/commuter/bootstrap/DevBootstrap.java
+++ src/main/java/com/jbabineau/commuter/bootstrap/DevBootstrap.java
@@ -1,6 +1,8 @@
 package com.jbabineau.commuter.bootstrap;
 
+import com.jbabineau.commuter.data.entities.Person;
 import com.jbabineau.commuter.data.entities.TripType;
+import com.jbabineau.commuter.data.repositories.PersonRepository;
 import com.jbabineau.commuter.data.repositories.TripTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
     private TripTypeRepository tripTypeRepository;
+    private PersonRepository personRepository
 
     @Autowired
-    public DevBootstrap(TripTypeRepository tripTypeRepository) {
+    public DevBootstrap(TripTypeRepository tripTypeRepository, PersonRepository personRepository) {
         this.tripTypeRepository = tripTypeRepository;
+        this.personRepository = personRepository;
     }
 
     @Override
@@ -28,6 +32,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         tripTypeRepository.save(new TripType("Bus", "You are riding a bus."));
         tripTypeRepository.save(new TripType("Ferry", "You're on a boat and you're riding fast."));
         tripTypeRepository.save(new TripType("Walking", "Getting around on those Sketchers of yours."));
+
+        personRepository.save(new Person("Jimmy", "Babs", "babineau.james@gmail.com","001" ));
+        personRepository.save(new Person("Denwa", "Griffon", "denwa.griffon@gmail.com","002" ));
+        personRepository.save(new Person("Zak", "Attack", "ZakAttack003@gmail.com","003" ));
+        
     }
 
 
